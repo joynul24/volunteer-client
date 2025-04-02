633652ce56be51cf390f74db1ce9eb5476a43aa2
joynul24 <joynula919@gmail.com>
2025-04-02 19:02:35 +0600

    added firebase authontication

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCgwcvqWJDSnM4tXJ5GFYaXMDBGx0gG9-M",
-  authDomain: "volunteer-hub-76b8d.firebaseapp.com",
-  projectId: "volunteer-hub-76b8d",
-  storageBucket: "volunteer-hub-76b8d.firebasestorage.app",
-  messagingSenderId: "933578495002",
-  appId: "1:933578495002:web:32c05fc884ba83fff02da3"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
